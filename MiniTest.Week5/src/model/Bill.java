dff6c887cea8ef75d400d766b449ebbdcaaca54b
Compass <ndphong165@gmail.com>
2022-09-04 05:31:28 +0700

    done model

---
+++ MiniTest.Week5/src/model/Bill.java
@@ -0,0 +1,46 @@
+package model;
+
+import java.io.Serializable;
+
+public class Bill implements Serializable,Calculated {
+    private long oldIndex;
+    private long newIndex;
+
+    public Bill() {
+    }
+    public Bill(long oldIndex, long newIndex) {
+        this.oldIndex = oldIndex;
+        this.newIndex = newIndex;
+
+    }
+    public long getOldIndex() {
+        return oldIndex;
+    }
+
+    public void setOldIndex(long oldIndex) {
+        this.oldIndex = oldIndex;
+    }
+
+    public long getNewIndex() {
+        return newIndex;
+    }
+
+    public void setNewIndex(long newIndex) {
+        this.newIndex = newIndex;
+    }
+
+
+    public long payMoney() {
+        long index = newIndex - oldIndex;
+        long money = index * index;
+        return money;
+    }
+
+    @Override
+    public String toString() {
+        return "Bill{" +
+                "oldIndex=" + oldIndex +
+                ", newIndex=" + newIndex +
+                '}';
+    }
+}
---
+++ MiniTest.Week5/src/model/Customers.java
@@ -0,0 +1,62 @@
+package model;
+
+
+import java.io.Serializable;
+
+public class Customers implements Serializable {
+    private String name;
+    private int number;
+    private String electricMeterId;
+
+    private Bill bill;
+
+    public Customers() {
+    }
+
+    public Customers(String name, int number, String electricMeterId, Bill bill) {
+        this.name = name;
+        this.number = number;
+        this.electricMeterId = electricMeterId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getElectricMeterId() {
+        return electricMeterId;
+    }
+
+    public void setElectricMeterId(String electricMeterId) {
+        this.electricMeterId = electricMeterId;
+    }
+
+    public Bill getBill() {
+        return bill;
+    }
+
+    public void setBill(Bill bill) {
+        this.bill = bill;
+    }
+
+    @Override
+    public String toString() {
+        return "customers{" +
+                "name='" + name + '\'' +
+                ", number=" + number +
+                ", electricMeterId='" + electricMeterId + '\'' +
+                '}';
+    }
+}
